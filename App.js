105f67088a8dcca3fb4c50b15de047814f441a55
sankalp <160774479+SankalpYerigeri@users.noreply.github.com>
2024-05-28 19:45:19 +0530

    Renderd a member card

--- App.js
+++ App.js
@@ -30,7 +30,33 @@ const Body = () =>
             <div className="body-container">
                 <h2 className="team-page-heading">MEET OUR TEAM</h2>
                 <div className="search">Search</div>
+                <div className="team-container">
+                <Team/>
+                <Team/>
+                <Team/>
+                <Team/>
+                <Team/>
+                <Team/>
+                <Team/>
+                <Team/>
+                </div>
             </div>
+            </>
+        )
+    }
+
+const Team = () =>
+    {
+        return(
+            <>
+            <div class="team-card">
+                <img src="https://raw.githubusercontent.com/Prathibimba19/React_Web/main/IMG-20240504-WA0019.jpg" alt="Team Member" class="team-img"/>
+                <div class="team-info">
+                    <h3 class="team-name">Bhavana R</h3>
+                    <p class="team-role">Senior Artist</p>
+                <div/>
+            </div>
+</div>
             </>
         )
     }
@@ -40,6 +66,7 @@ const AppLayout = () =>
         return(<>
             <Header/>
             <Body/>
+            
             </>
         )
     }
